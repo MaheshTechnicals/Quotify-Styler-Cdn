a840aff65c601bfd6ba2b9bce36f793e61b106da
MaheshTechnicals <msvarma10001@gmail.com>
2024-08-24 14:35:20 +0530

    update api.js

--- public/api.js
+++ public/api.js
@@ -97,5 +97,12 @@ let allData = [
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/MaheshTechnicals/Quotify-Styler-Cdn/styles/style-11/app.css"/>`,
     js: `<script src="https://cdn.jsdelivr.net/gh/MaheshTechnicals/Quotify-Styler-Cdn/styles/style-11/app.js"></script>`
   },
+  {
+    name: "Style 12: Reaction Mode.",
+    img: "https://raw.githubusercontent.com/MaheshTechnicals/Quotify-Styler-Cdn/main/res/posters/ban12.jpg",
+    css: `<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/MaheshTechnicals/Quotify-Styler-Cdn/styles/style-12/app.css"/>`,
+    js: `<script src="https://cdn.jsdelivr.net/gh/MaheshTechnicals/Quotify-Styler-Cdn/styles/style-12/app.js"></script>`
+    },
 
 ]
