9bee4fbed140af7a728972bea4f03153005035c5
MaheshTechnicals <msvarma10001@gmail.com>
2024-12-09 12:26:32 +0530

    update api.js : Switch to jsdelivr to netlify servers

--- api/api.js
+++ api/api.js
@@ -4,8 +4,8 @@ let allData = [
     name: "Style 1: Red Bull Mode.",
     img: "https://raw.githubusercontent.com/MaheshTechnicals/Quotify-Styler-Cdn/main/res/posters/ban1.jpg",
     css: `<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/MaheshTechnicals/Quotify-Styler-Cdn/styles/style-1/app.css"/>`,
-    js: `<script src="https://cdn.jsdelivr.net/gh/MaheshTechnicals/Quotify-Styler-Cdn/styles/style-1/app.js"></script>`
+<link rel="stylesheet" href="https://quotify-styler.netlify.app/styles/style-1/app.css"/>`,
+    js: `<script src="https://quotify-styler.netlify.app/styles/style-1/app.js"></script>`
   },
 
 
@@ -15,8 +15,8 @@ let allData = [
     name: "Style 2: Dark Mode.",
     img: "https://raw.githubusercontent.com/MaheshTechnicals/Quotify-Styler-Cdn/main/res/posters/ban2.jpg",
     css: `<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/MaheshTechnicals/Quotify-Styler-Cdn/styles/style-2/app.css"/>`,
-    js: `<script src="https://cdn.jsdelivr.net/gh/MaheshTechnicals/Quotify-Styler-Cdn/styles/style-2/app.js"></script>`
+<link rel="stylesheet" href="https://quotify-styler.netlify.app/styles/style-2/app.css"/>`,
+    js: `<script src="https://quotify-styler.netlify.app/styles/style-2/app.js"></script>`
   },
 
 
@@ -24,8 +24,8 @@ let allData = [
     name: "Style 3: Light Gray Mode.",
     img: "https://raw.githubusercontent.com/MaheshTechnicals/Quotify-Styler-Cdn/main/res/posters/ban3.jpg",
     css: `<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/MaheshTechnicals/Quotify-Styler-Cdn/styles/style-2/app.css"/>`,
-    js: `<script src="https://cdn.jsdelivr.net/gh/MaheshTechnicals/Quotify-Styler-Cdn/styles/style-3/app.js"></script>`
+<link rel="stylesheet" href="https://quotify-styler.netlify.app/styles/style-2/app.css"/>`,
+    js: `<script src="https://quotify-styler.netlify.app/styles/style-3/app.js"></script>`
   },
 
 
@@ -33,8 +33,8 @@ let allData = [
     name: "Style 4: Material Blue Mode.",
     img: "https://raw.githubusercontent.com/MaheshTechnicals/Quotify-Styler-Cdn/main/res/posters/ban4.jpg",
     css: `<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/MaheshTechnicals/Quotify-Styler-Cdn/styles/style-4/app.css"/>`,
-    js: `<script src="https://cdn.jsdelivr.net/gh/MaheshTechnicals/Quotify-Styler-Cdn/styles/style-4/app.js"></script>`
+<link rel="stylesheet" href="https://quotify-styler.netlify.app/styles/style-4/app.css"/>`,
+    js: `<script src="https://quotify-styler.netlify.app/styles/style-4/app.js"></script>`
   },
 
 
@@ -42,16 +42,16 @@ let allData = [
     name: "Style 5: White Mode.",
     img: "https://raw.githubusercontent.com/MaheshTechnicals/Quotify-Styler-Cdn/main/res/posters/ban5.jpg",
     css: `<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/MaheshTechnicals/Quotify-Styler-Cdn/styles/style-5/app.css"/>`,
-    js: `<script src="https://cdn.jsdelivr.net/gh/MaheshTechnicals/Quotify-Styler-Cdn/styles/style-5/app.js"></script>`
+<link rel="stylesheet" href="https://quotify-styler.netlify.app/styles/style-5/app.css"/>`,
+    js: `<script src="https://quotify-styler.netlify.app/styles/style-5/app.js"></script>`
   },
 
   {
     name: "Style 6: Rocking Buble Animated",
     img: "https://raw.githubusercontent.com/MaheshTechnicals/Quotify-Styler-Cdn/main/res/posters/ban6.jpg",
     css: `<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/MaheshTechnicals/Quotify-Styler-Cdn/styles/style-6/app.css"/>`,
-    js: `<script src="https://cdn.jsdelivr.net/gh/MaheshTechnicals/Quotify-Styler-Cdn/styles/style-6/app.js"></script>`
+<link rel="stylesheet" href="https://quotify-styler.netlify.app/styles/style-6/app.css"/>`,
+    js: `<script src="https://quotify-styler.netlify.app/styles/style-6/app.js"></script>`
   },
 
 
@@ -59,8 +59,8 @@ let allData = [
     name: "Style 7: Material Purple Mode",
     img: "https://raw.githubusercontent.com/MaheshTechnicals/Quotify-Styler-Cdn/main/res/posters/ban7.jpg",
     css: `<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/MaheshTechnicals/Quotify-Styler-Cdn/styles/style-7/app.css"/>`,
-    js: `<script src="https://cdn.jsdelivr.net/gh/MaheshTechnicals/Quotify-Styler-Cdn/styles/style-7/app.js"></script>`
+<link rel="stylesheet" href="https://quotify-styler.netlify.app/styles/style-7/app.css"/>`,
+    js: `<script src="https://quotify-styler.netlify.app/styles/style-7/app.js"></script>`
   },
 
 
@@ -68,8 +68,8 @@ let allData = [
     name: "Style 8: Dark Purple Mode.",
     img: "https://raw.githubusercontent.com/MaheshTechnicals/Quotify-Styler-Cdn/main/res/posters/ban8.jpg",
     css: `<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/MaheshTechnicals/Quotify-Styler-Cdn/styles/style-8/app.css"/>`,
-    js: `<script src="https://cdn.jsdelivr.net/gh/MaheshTechnicals/Quotify-Styler-Cdn/styles/style-8/app.js"></script>`
+<link rel="stylesheet" href="https://quotify-styler.netlify.app/styles/style-8/app.css"/>`,
+    js: `<script src="https://quotify-styler.netlify.app/styles/style-8/app.js"></script>`
   },
 
 
@@ -77,8 +77,8 @@ let allData = [
     name: "Style 9: Multi Color Mode.",
     img: "https://raw.githubusercontent.com/MaheshTechnicals/Quotify-Styler-Cdn/main/res/posters/ban9.jpg",
     css: `<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/MaheshTechnicals/Quotify-Styler-Cdn/styles/style-9/app.css"/>`,
-    js: `<script src="https://cdn.jsdelivr.net/gh/MaheshTechnicals/Quotify-Styler-Cdn/styles/style-9/app.js"></script>`
+<link rel="stylesheet" href="https://quotify-styler.netlify.app/styles/style-9/app.css"/>`,
+    js: `<script src="https://quotify-styler.netlify.app/styles/style-9/app.js"></script>`
   },
 
 
@@ -86,23 +86,23 @@ let allData = [
     name: "Style 10: Shinier Yellow Mode.",
     img: "https://raw.githubusercontent.com/MaheshTechnicals/Quotify-Styler-Cdn/main/res/posters/ban10.jpg",
     css: `<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/MaheshTechnicals/Quotify-Styler-Cdn/styles/style-10/app.css"/>`,
-    js: `<script src="https://cdn.jsdelivr.net/gh/MaheshTechnicals/Quotify-Styler-Cdn/styles/style-10/app.js"></script>`
+<link rel="stylesheet" href="https://quotify-styler.netlify.app/styles/style-10/app.css"/>`,
+    js: `<script src="https://quotify-styler.netlify.app/styles/style-10/app.js"></script>`
   },
 
   {
     name: "Style 11: Khaki Mode.",
     img: "https://raw.githubusercontent.com/MaheshTechnicals/Quotify-Styler-Cdn/main/res/posters/ban11.jpg",
     css: `<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/MaheshTechnicals/Quotify-Styler-Cdn/styles/style-11/app.css"/>`,
-    js: `<script src="https://cdn.jsdelivr.net/gh/MaheshTechnicals/Quotify-Styler-Cdn/styles/style-11/app.js"></script>`
+<link rel="stylesheet" href="https://quotify-styler.netlify.app/styles/style-11/app.css"/>`,
+    js: `<script src="https://quotify-styler.netlify.app/styles/style-11/app.js"></script>`
   },
   {
     name: "Style 12: Reaction Mode.",
     img: "https://raw.githubusercontent.com/MaheshTechnicals/Quotify-Styler-Cdn/main/res/posters/ban12.jpg",
     css: `<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/MaheshTechnicals/Quotify-Styler-Cdn/styles/style-12/app.css"/>`,
-    js: `<script src="https://cdn.jsdelivr.net/gh/MaheshTechnicals/Quotify-Styler-Cdn/styles/style-12/app.js"></script>`
+  <link rel="stylesheet" href="https://quotify-styler.netlify.app/styles/style-12/app.css"/>`,
+    js: `<script src="https://quotify-styler.netlify.app/styles/style-12/app.js"></script>`
     },
 
 ]
